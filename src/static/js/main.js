54fe6d72ca05ee317e1b1f83aa83b2903e7c4093
nicomiguelino <nicomiguelino2014@gmail.com>
2023-09-03 00:02:21 -0700

    feat: Display latest RSS feed.

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,47 @@
-window.onload = function() {
-  console.log('Hello World!');
-}
+const processUrl = (url, enableCors) => {
+  const corsProxyUrl = 'https://cors-anywhere.herokuapp.com';
+
+  if (enableCors) {
+    return `${corsProxyUrl}/${url}`;
+  } else {
+    return url;
+  }
+};
+
+const getApiResponse = () => {
+  return new Promise((resolve, reject) => {
+    let parser = new RSSParser();
+    const url = processUrl('https://www.reddit.com/.rss', true);
+    parser.parseURL(url, (err, feed) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve(feed.items);
+    });
+  });
+};
+
+const getRssData = function() {
+  return {
+    entries: [],
+    init: async function() {
+      const data = await getApiResponse();
+
+      setInterval(await (async () => {
+        const lambda = () => {
+          const limit = Math.floor(Math.random() * 5) + 1;
+          this.entries = data.slice(0, limit);
+        };
+
+        lambda();
+
+        return lambda;
+      })(), 3000);
+    },
+  };
+};
+
+document.addEventListener('alpine:init', () => {
+  Alpine.data('rss', getRssData);
+});
